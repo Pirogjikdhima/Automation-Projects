0ec4a0dc31855b279ca4b0cc57e1e4a82500ad8f
Piro Gjikdhima <111192631+Pirogjikdhima@users.noreply.github.com>
2025-04-01 15:32:15 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -1,25 +1,20 @@
-import requests 
-import random
+"""This module fetches and stores random quotes."""
 
+import random
+import requests
 
 def get_quote_from_api() -> str:
-    """Fetches a random quote from the API and appends it to a file.
-
-    Returns:
-        str: A formatted quote string.
-    """
-    response = requests.get("https://zenquotes.io/api/random").json()
-    quote = f"\"{response[0]['q']}-{response[0]['a']}\""
-    with open("quotes.txt", mode='a') as file:
+    """Fetches a random quote from the API and appends it to a file."""
+    response = requests.get("https://zenquotes.io/api/random", timeout=5).json()
+    quote = f"\"{response[0]['q']} - {response[0]['a']}\""
+    
+    with open("quotes.txt", mode="a", encoding="utf-8") as file:
         file.write(quote + "\n")
+
     return quote
 
 def get_quote_from_file() -> str:
-    """Retrieves a random quote from a file.
-
-    Returns:
-        str: A random quote from the file.
-    """
+    """Retrieves a random quote from a file."""
     with open("quotes.txt", mode="r", encoding="utf-8") as file:
         lines = [line.strip() for line in file.readlines()]
         return random.choice(lines)
