2fb06364616e95d7c67bc65e654e712ff96b5a99
Piro Gjikdhima <111192631+Pirogjikdhima@users.noreply.github.com>
2025-04-01 15:34:00 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -7,7 +7,7 @@ def get_quote_from_api() -> str:
     """Fetches a random quote from the API and appends it to a file."""
     response = requests.get("https://zenquotes.io/api/random", timeout=5).json()
     quote = f"\"{response[0]['q']} - {response[0]['a']}\""
-    
+
     with open("quotes.txt", mode="a", encoding="utf-8") as file:
         file.write(quote + "\n")
 
